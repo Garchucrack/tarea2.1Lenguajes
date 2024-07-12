ffb70d8bba1591e41a796a1a373048139320c5dc
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-07-12 15:07:29 -0600

    implementacion de los controladores globales para los campos de texto del correo y la cotrasena en la clase LoginScreenState en el login_screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -9,5 +9,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 }
