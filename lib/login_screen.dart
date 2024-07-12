bdf709d9c883b87642a1358ca3ba25e46468eba8
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-07-12 14:59:12 -0600

    importacion de librerias paara el login_screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'register_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'login_screen.dart';
-
-void main() {
-  runApp(MyApp());
-}
