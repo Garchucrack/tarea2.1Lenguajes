f05958c2fe7a3380fdb6fb7ab9194a79ae3ca50c
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-07-11 21:28:52 -0600

    configuracion de personalizacion global para el appbar e importacion de fuentes de de texto de google

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,30 @@ import 'login_screen.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+
+  
+        textTheme: GoogleFonts.vampiroOneTextTheme(Theme.of(context).textTheme),
+
+     
+       
+
+       
+        scaffoldBackgroundColor: const Color.fromARGB(255, 92, 91, 91), 
+
+       
+        appBarTheme: AppBarTheme(
+          color: Colors.black, 
+          iconTheme: IconThemeData(color: Colors.white), toolbarTextStyle: GoogleFonts.vampiroOneTextTheme(Theme.of(context).textTheme).apply(bodyColor: Colors.white).bodyMedium, titleTextStyle: GoogleFonts.vampiroOneTextTheme(Theme.of(context).textTheme).apply(bodyColor: Colors.orange ).titleLarge, 
+        ),
+      ),
+      home: LoginScreen(),
+    );
+  
