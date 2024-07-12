19a7963d12cfea54682766ec07613529369f2e6a
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-07-12 15:04:35 -0600

    creacion de las clases LoginScreen y LoginScreenState en el login_screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,3 +2,12 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'register_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+}
