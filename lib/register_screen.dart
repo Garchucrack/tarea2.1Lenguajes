5367d7bb9b5121adc1d114999c886575b574e76d
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-07-12 19:03:28 -0600

    Agregar los demas TextFormField con su controladores y validadores para los demas campos de texto en el register_screen

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -56,11 +56,58 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   return null;
                 },
               ),
-             
-             
-             
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Correo'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor ingresa tu correo';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+$').hasMatch(value) || !value.endsWith('.edu.hn')) {
+                    return 'Por favor ingresa un correo válido que termine en .edu.hn';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _phoneController,
+                decoration: InputDecoration(labelText: 'Teléfono'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor ingresa tu teléfono';
+                  } else if (!RegExp(r'^[39]\d{7}$').hasMatch(value)) {
+                    return 'El teléfono debe comenzar con 3 o 9 y tener exactamente 8 dígitos';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Contraseña'),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor ingresa tu contraseña';
+                  } else if (value.length < 8 || !RegExp(r'^(?=.*[A-Z])(?=.*\W)').hasMatch(value)) {
+                    return 'La contraseña debe tener al menos 8 caracteres, incluyendo una mayúscula y un carácter especial';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _confirmPasswordController,
+                decoration: InputDecoration(labelText: 'Confirmar Contraseña'),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor confirma tu contraseña';
+                  } else if (value != _passwordController.text) {
+                    return 'Las contraseñas no coinciden';
+                  }
+                  return null;
+                },
+              ),
               
-             
+              
             ],
           ),
         ),
