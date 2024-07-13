a1af60b7e813a42d0768a7d7f80ffe3e35ad9e5a
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-07-12 18:33:13 -0600

    Creacion de las clases RegisterScreen y _RegisterScreenState y sus controladores para los campos de texto y por ultimo un widget buildcontext de en el register_screen

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,1 +1,25 @@
 
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+    );
+  }
+}
