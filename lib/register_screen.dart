cad65beccd11b81cfe27da74e3bba050b0254625
Garchucrack <146801862+Garchucrack@users.noreply.github.com>
2024-07-12 18:37:07 -0600

    Creacion de el  metodo _register para validar el formulario para verificar si el texto ingresado es el correcto o no  en el register_screen

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -14,7 +14,17 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
 
- 
+  void _register() {
+    if (_formKey.currentState!.validate()) {
+      final name = _nameController.text;
+      final email = _emailController.text;
+      final phone = _phoneController.text;
+      final password = _passwordController.text;
+      final confirmPassword = _confirmPasswordController.text;
+
+     
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
